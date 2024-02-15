48b7f82752b0b364936e9f7beb291c804a515b24
AlisaVyzhimova <159880117+AlisaVyzhimova@users.noreply.github.com>
2024-02-15 12:13:31 +0300

    Create практm.py

---
+++ практm.py
@@ -0,0 +1,24 @@
+a=input("введите А")
+a=int(a)
+if a>0:
+    print("a больше нуля")
+
+задание 2.
+a=input("введитe А")
+a=int(a)
+if a>0:
+    print(1)
+else:
+    print("-1")
+
+задание 3.
+a=input()
+if a==str:
+    print("строка")
+    if a=="Hello World":
+        print ("мир")
+    elif a=="1":
+        print("число 1")
+else:
+    print("а не строка")
+
