01830cf1622f32ba8d7196ac3d7238d30d89114a
AlisaVyzhimova <159880117+AlisaVyzhimova@users.noreply.github.com>
2024-04-04 12:07:36 +0300

    Update практическая 04.04.py

--- практическая 04.04.py
+++ практическая 04.04.py
@@ -27,6 +27,33 @@ print(m)
       
 #задача 36
 #задача 37
+import random
+def creatArray():
+    print('Input first index matrix: ')
+    x = int(input())
+    print('Input second index matrix: ')
+    y = int(input())
+    array = []
+    for i in range(x):
+        array.append([])
+        for j in range(y):
+            array[i].append(random.randint(-1,1))
+    return array
+a=int(0)
+m=[]
+matrix=creatArray()
+print(matrix)
+for i in matrix: 
+    for t in i:
+        if t!=0:
+            a+=1
+            
+    print(a)
+    m.append(a)
+    a=0
+print(m)
+        
+        
 #задача 38
 #задача 39
 #задача 40
